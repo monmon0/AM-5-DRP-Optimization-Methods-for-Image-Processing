c0e59af54f180b5eb99a50fa421f744ad5a7262c
Ha Dang Vu <127801780+hdangvu@users.noreply.github.com>
2024-10-10 17:57:24 -0400

    Add files via upload

---
+++ method/solve_L1_FISTA.m
@@ -0,0 +1,36 @@
+function [u_denoised, residuals] = solve_L1_FISTA(u_true, u_noise, lambda, tolerance)
+
+    L = 1.0; 
+    
+    u_k = u_noise;  
+    u_prev = u_noise;  % u^{k-1}
+    v = u_k;  % Acceleration variable v
+    t_k = 1;  % Initial step size
+    residuals = [];  % To store residuals
+    residual = tolerance + 1;  % Initial residual set above tolerance
+    
+    while residual > tolerance
+        grad_g_v = v - u_true;
+        
+        t_k_new = (1 + sqrt(1 + 4*t_k^2)) / 2;
+        
+        beta = v - (1/L) * grad_g_v;  
+        u_k = soft_threshold(beta, lambda / L);  
+        
+        residual = norm(u_k - u_prev);
+        residuals = [residuals, residual];
+        
+        v = u_k + ((t_k - 1) / t_k_new) * (u_k - u_prev);
+        t_k = t_k_new;
+        
+        u_prev = u_k;
+    end
+
+    u_denoised = u_k;
+    disp(residuals);
+    
+end
+
+function result = soft_threshold(beta, thresh)
+    result = sign(beta) .* max(abs(beta) - thresh, 0);
+end
